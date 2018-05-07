e336017a5a8e8498a513b02cd590581bb9619a8b
melodyVoid <botaoxyz@gmail.com>
2018-05-07 13:54:18 +0800

    fix:bug

--- packages/messageBox/index.js
+++ packages/messageBox/index.js
@@ -31,6 +31,6 @@ MessageBox.alert = (options = { title: '', icon: '', message: '', size: ''}) => 
 MessageBox.close = () => {
   // component.$data.isShow = false
   const oMsg = document.querySelector('.be-msg-box-wrapper')
-  oMsg.style.display = 'none'
+  oMsg && (oMsg.style.display = 'none')
 }
 export default MessageBox
